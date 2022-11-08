06e69aa13edd88addec6855f5c2a4ffd130c085b
ZeharaY <106754679+zeharaY@users.noreply.github.com>
2022-11-08 19:47:24 +0300

    com
    com

---
+++ sort.h
@@ -0,0 +1,15 @@
+#ifndef SORT_H
+#define SORT_H
+#include <stdio.h>
+
+/*Prototypes*/
+
+void bubble_sort(int *array, size_t size);
+
+void insertion_sort_list(listint_t **list);
+
+void selection_sort(int *array, size_t size);
+
+void quick_sort(int *array, size_t size);
+
+#endif
